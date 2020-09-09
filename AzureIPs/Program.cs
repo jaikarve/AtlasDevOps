8c5acba0f45b6d0283dda86b9158a135dd85dc63
Prashant Gupta <prashant.gupta@mongodb.com>
2020-09-08 18:57:51 -0700

    initial commit

---
+++ AzureIPs/Program.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+
+
+namespace azureipaddr
+{
+    public class Whitelist {
+        private string _ipAddress;
+        //private string _comment;
+
+        public string ipAddress { get => _ipAddress; set => _ipAddress = value; }
+        //public string comment { get => _comment; set => _comment = value; }
+    }
+ class Program
+    {
+        // Path to the locally saved weekly file
+        const string weeklyFilePath = @"C:\Users\pras_\Downloads\ServiceTags_Public_20200824.json";
+
+        static void Main(string[] args)
+        {
+            // United States geography has the following regions:
+            // Central US, East US 2, East US, North Central US, 
+            // South Central US, West Central US, West US, West US 2
+            List<Whitelist> warray = new List<Whitelist>();
+            List<string> USGeographyRegions = new List<string>
+            {
+//                "centralus",
+                "eastus",
+                "eastus2",
+                "northcentralus",
+                "southcentralus",
+                "westcentralus",
+                "westus",
+                "westus2"
+            };
+
+            // Load the weekly file
+            JObject weeklyFile = JObject.Parse(File.ReadAllText(weeklyFilePath));
+            JArray values = (JArray)weeklyFile["values"];
+
+            foreach (string region in USGeographyRegions)
+            {
+                string azureCloudRegion = $"AzureCloud.{region}";
+                Console.WriteLine(azureCloudRegion);
+
+                var ipList =
+                    from v in values
+                    where (string)v["name"] == azureCloudRegion
+                    select v["properties"]["addressPrefixes"];
+
+                foreach (var ip in ipList.Children())
+                {
+                    Whitelist wlist = new Whitelist();
+                    wlist.ipAddress = ip.Value<string>();
+                   // wlist.comment = azureCloudRegion;
+                    
+                    warray.Add(wlist);
+                }
+                
+            }
+            Console.WriteLine(warray.Count);
+            var json = JsonSerializer.Serialize(warray);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+# A simple example to show how to deploy Atlas Cluster using Azure DevOps and Terraform provider
+
+## Setup Azure DevOps Organization and project
+
+### Navigate to Project, Choose Pipelines
+
+### Choose Create Pipeline
+
+### Where is your code - Choose Github
+
+### Fork this repo
+
+### Choose repo you forked in previous step, It will load the azure-pipelines.yml file
+
+#### Create Following variables
+
+1. $(ContainerName)
+2. $(ResourceGroupName)
+3. $(StorageAccountName)
+
+#### Update yml file value (Use the value of backendserviceArm template)
+##### backendServiceArm: 'Visual Studio Enterprise (62626e97-807f-4c32-98e6-b7c3078adaf9)'
+
+#### Configure Atlas API Acces following these links
+
+https://docs.atlas.mongodb.com/configure-api-access/
+
+#### Ensure IPs of Azure region of your microsoft hosted or self hosted agent is whitelisted in the Atlas programmatic key created above
+
+#### Go to Library of left hand navigation
+#### upload terraform.tfvars file with following values
+
+
+> public_key = "<Your Atlas Public Key>"
+>
+> private_key  = "<Your Atlas Private Key>"
+>
+> atlasprojectid = "<Atlas project Id>"
+>
+> cluster_region = "US_WEST_2"
+>
+> atlas_provider_name = "AWS"
+>
+> atlas_provider_instance_size_name = "M10"
+>
+> auto_scaling_disk_gb_enabled = true
+>
+> mongo_db_major_version       = "4.4"
+>
+> mongodb_atlas_database_username = "<username you want to use>"
+>
+> mongodb_atlas_database_user_password = "<password you want to use>"
+
+#### Click on terrafornm.tvars file name can choose Pipeline Permissions - Authorize for use in all pipelines (Else you will be asked to permit when you run the piepline first time)
+
+#### After this, Come back to pipelines and say run pipeline to trigger it manually
+
+
